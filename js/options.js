8aefdf07be623b83bbb74abbab2cc1f3c1ca69e0
zhangpan <zp@yunshipei.com>
2014-09-28 01:34:15 +0800

    options page ready

--- js/options.js
+++ js/options.js
@@ -1,4 +1,5 @@
 var engine = ["sogou", "baidu", "google"];
+
 function $(selector) {
 	return document.querySelector(selector);
 }
@@ -6,6 +7,7 @@ function $(selector) {
 function $$(selector) {
 	return document.querySelectorAll(selector);
 }
+
 function toArray(obj) {
 	return Array.prototype.slice.call(obj);
 }
@@ -14,20 +16,21 @@ var defaultObj = {
 	"search": true,
 	"engine": engine[0],
 	"translate": true,
+	"disableAll": false
 };
 var obj = {};
 //渲染
 function defaulChecked(obj) {
 	//清空checked
-	var checkedItem = $$("input[checked]");
-	checkedItem&&toArray(checkedItem).forEach(function(item) {
-		item.removeAttribute("checked");
+	var checkedItem = $$("input");
+	checkedItem && toArray(checkedItem).forEach(function(item) {
+		item["checked"] = false;
 	});
 	for (var prop in obj) {
 		if (prop !== "engine") {
-			$("#" + prop).setAttribute("checked", "");
+			$("#" + prop)["checked"] = (prop == "disableAll" ? false : true);
 		} else {
-			$("#" + obj[prop]).setAttribute("checked", "");
+			$("#" + obj[prop])["checked"] = true;
 		}
 	}
 };
@@ -37,30 +40,25 @@ setMuti.addEventListener("click", function(e) {
 	var targetLi = e.target.parentNode;
 	if (targetLi.tagName === 'LI') {
 		var firstChild = targetLi.children[0];
-		checkToggle(e, firstChild);
-		obj[firstChild.id] = obj[firstChild.id] ? false : true;
+		obj[firstChild.id] = !obj[firstChild.id];
 	}
 });
 
 $("#disableAll").addEventListener("click", function(e) {
-	var firstChild = e.currentTarget.children[0];
-	checkToggle(e, firstChild);
 	obj["disableAll"] = !obj["disableAll"];
-	setMuti.className = setMuti.className? "":"mask";
+	setMuti.className = setMuti.className ? "" : "mask";
 	//重绘
-	obj["disableAll"]?defaulChecked({"engine":engine[0]}):defaulChecked(defaultObj);
+	obj["disableAll"] ? defaulChecked({
+		"engine": engine[0]
+	}) : defaulChecked(defaultObj);
 });
 
 $("#engine").addEventListener("click", function(e) {
 	e.stopPropagation();
 	var engineList = $$("#engine input");
 	toArray(engineList).forEach(function(item) {
-		item.removeAttribute("checked");
+		item["checked"] = false;
 	});
-	engineList[Math.ceil(Array.prototype.indexOf.call(this.children, e.target) / 2)].setAttribute("checked", "");
+	engineList[Math.ceil(Array.prototype.indexOf.call(this.children, e.target) / 2)]["checked"] = true;
 	obj["engine"] = e.target['id' || 'for'];
 });
-
-function checkToggle(e, firstChild) {
-	firstChild.hasAttribute("checked") ? firstChild.removeAttribute("checked") : firstChild.setAttribute("checked", "");
-}
