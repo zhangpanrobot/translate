e2ef49d20f978606ad79afb3c6b737880e240089
zp <zhangpanrobot@gmail.com>
2014-09-28 12:53:08 +0800

    options has done

--- js/options.js
+++ js/options.js
@@ -1,4 +1,5 @@
 var engine = ["sogou", "baidu", "google"];
+var setMuti = $("#setting");
 
 function $(selector) {
 	return document.querySelector(selector);
@@ -19,6 +20,10 @@ var defaultObj = {
 	"disableAll": false
 };
 var obj = {};
+chrome.storage.local.get(null, function(item) {
+	obj = Object.keys(item).length ? item : defaultObj;
+	defaulChecked(obj);
+});
 //渲染
 function defaulChecked(obj) {
 	//清空checked
@@ -27,30 +32,46 @@ function defaulChecked(obj) {
 		item["checked"] = false;
 	});
 	for (var prop in obj) {
-		if (prop !== "engine") {
-			$("#" + prop)["checked"] = (prop == "disableAll" ? false : true);
+		if (obj.disableAll) {
+			if (prop !== "engine") {
+				$("#" + prop)["checked"] = false;
+				$("#disableAll")["checked"] = true; 
+				setMuti.className = "mask";
+			} else {
+				$("#" + obj[prop])["checked"] = false;
+				$("#sogou")["checked"] = true;
+			}
 		} else {
-			$("#" + obj[prop])["checked"] = true;
+			if(prop !== "engine") {
+				$("#" + prop)["checked"] = obj[prop];
+				setMuti.className = "";
+			} else {
+				$("#sogou")["checked"] = false;
+				$("#" + obj[prop])["checked"] = true;
+			}
 		}
 	}
 };
-defaulChecked(defaultObj);
-var setMuti = $("#setting");
+
 setMuti.addEventListener("click", function(e) {
 	var targetLi = e.target.parentNode;
 	if (targetLi.tagName === 'LI') {
 		var firstChild = targetLi.children[0];
 		obj[firstChild.id] = !obj[firstChild.id];
 	}
+	chrome.storage.local.set(obj);
 });
 
 $("#disableAll").addEventListener("click", function(e) {
-	obj["disableAll"] = !obj["disableAll"];
+	obj.disableAll = !obj.disableAll;
+	obj = obj.disableAll?obj: defaultObj;
+	if(obj.disableAll){
+		obj.engine = "sogou";
+	}
 	setMuti.className = setMuti.className ? "" : "mask";
 	//重绘
-	obj["disableAll"] ? defaulChecked({
-		"engine": engine[0]
-	}) : defaulChecked(defaultObj);
+	defaulChecked(obj);
+	chrome.storage.local.set(obj);
 });
 
 $("#engine").addEventListener("click", function(e) {
@@ -61,4 +82,5 @@ $("#engine").addEventListener("click", function(e) {
 	});
 	engineList[Math.ceil(Array.prototype.indexOf.call(this.children, e.target) / 2)]["checked"] = true;
 	obj["engine"] = e.target['id' || 'for'];
+	chrome.storage.local.set(obj);
 });
