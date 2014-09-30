87cba85d78bf0aec0c0b907d73e0bdcf35346bc3
zp <zhangpanrobot@gmail.com>
2014-09-30 15:31:43 +0800

    chrome is ok

--- js/background.js
+++ js/background.js
@@ -9,6 +9,22 @@ chrome.runtime.onMessage.addListener(function(msg) {
 			document.execCommand('Copy');
 			input.remove();
 			break;
+		case 'translate':
+			var xhp = new XMLHttpRequest();
+			xhp.onreadystatechange = function() {
+				if (xhp.readyState == 4 && xhp.status == 200) { //xhp.responseText;
+					chrome.tabs.query({
+						active: true
+					}, function(tabs) {
+						chrome.tabs.sendMessage(tabs[0].id, {
+							cmd: 'translate-back',
+							data: xhp.responseText
+						});
+					});
+				}
+			}
+			xhp.open('GET', 'http://fanyi.youdao.com/openapi.do?keyfrom=sogou-inc&key=1309482814&type=data&doctype=json&version=1.1&q=' + msg.text, true);
+			xhp.send();
 		default:
 			return;
 	}
--- js/main.js
+++ js/main.js
@@ -6,22 +6,85 @@ function $(id) {
 function stopDefAction(evt) {
     evt.preventDefault();
 }
-
+var engine = ["sogou", "baidu", "google"];
+var searchLinkPre = ['http://www.sogou.com/web?query=', 'http://www.baidu.com/s?wd=', 'https://www.google.com.hk/?#newwindow=1&q='];
 //插入文档
-var isLink = true,ta;
+var isLink = false,
+    ta, copy, search, open, translate;
 chrome.storage.local.get(null, function(obj) {
     if (obj.disableAll) {
         return;
+    }
+    var board;
+
+    function sendBack(cmd, board) {
+        chrome.runtime.sendMessage({
+            cmd: cmd,
+            text: board
+        });
+    }
+    var translateWrapper = document.createElement("div");
+    var docfrag = document.createDocumentFragment();
+    var itemList = "<ul><li id='copy' class='" + (obj.copy ? "" : "muti-hide") + "'>复制</li><li id='search' class='" + (obj.search ? "" : "muti-hide") + "'><a target='_blank' href=''>搜索</a></li><li id='open' class='" + (isLink ? "" : "muti-hide") + "'><a target='_blank' href=''>打开</a></li></ul>";
+    var divWrapper = document.createElement("div");
+    divWrapper.id = "muti-translate";
+    divWrapper.className = "muti-hide";
+    divWrapper.innerHTML = itemList;
+    docfrag.appendChild(divWrapper);
+    document.body.appendChild(docfrag);
+    ta = document.getElementById("muti-translate");
+    open = document.getElementById("open");
+    search = document.getElementById("search");
+    //get selection(ie and chrome)
+    if (window.attachEvent) {
+        document.body.attachEvent("onmouseup", function(e) {
+            board = document.selection.createRange().text;
+            if ((e || event).srcElement.nodeName === "A") { //注入"打开"
+                console.log("haha");
+            }
+        });
     } else {
-        var docfrag = document.createDocumentFragment();
-        var itemList = "<ul><li id='copy' class='" + (obj.copy ? "" : "muti-hide") + "'>复制</li><li id='search' class='" + (obj.search ? "" : "muti-hide") + "'><a target='_blank' href=''>搜索</a></li><li id='open' class='" + (isLink ? "" : "muti-hide") + "'><a target='_blank' href=''>打开</a></li></ul>";
-        var divWrapper = document.createElement("div");
-        divWrapper.id = "muti-translate";
-        divWrapper.className = "muti-hide";
-        divWrapper.innerHTML = itemList;
-        docfrag.appendChild(divWrapper);
-        document.body.appendChild(docfrag);
-        ta = document.getElementById("muti-translate");
+        document.body.addEventListener("mouseup", function(e) {
+            if (document.getSelection().toString()) {
+                board = document.getSelection().toString();
+                ta.className = "";
+                ta.style.left = e.clientX + "px";
+                ta.style.top = e.clientY - "50" + "px";
+                isLink = (e.target.nodeName === "A");
+                if (isLink) {
+                    open.className = "";
+                    open.children[0].href = e.target.href;
+                }
+                if (obj.search) {
+                    search.children[0].href = searchLinkPre[engine.indexOf(obj.engine)] + board;
+                }
+                obj.translate ? sendBack('translate', board) : '';
+                ta.addEventListener("click", function(e) {
+                    switch (e.target.id) {
+                        case "copy":
+                            sendBack('copy', board);
+                            break;
+                        default:
+                            return;
+                    }
+                });
+
+                function dataResponse(data) {
+                    translateWrapper.textContent = JSON.stringify(data);
+                    ta.appendChild(translateWrapper);
+                }
+                chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+                    if (request.cmd == 'translate-back') {
+                        dataResponse(JSON.parse(request.data));
+                    }
+                });
+            } else {
+                ta.className = "muti-hide";
+            }
+        });
+        document.body.addEventListener("wheel", function(e) {
+            ta.className = "muti-hide";
+        });
     }
 });
 
@@ -39,36 +102,7 @@ chrome.storage.local.get(null, function(obj) {
     }*/
 
 //$("translate").addEventListener('mouseup', stopDefAction, false);
-var board; //board用来搜索及存到剪切板
-//get selection(ie and chrome)
-if (window.attachEvent) {
-    document.body.attachEvent("onmouseup", function(e) {
-        board = document.selection.createRange().text;
-        if ((e || event).srcElement.nodeName === "A") { //注入"打开"
-            console.log("haha");
-        }
-    });
-} else {
-    document.body.addEventListener("mouseup", function(e) {
-        if (document.getSelection().toString()) {
-            ta.className = "";
-            ta.style.left = e.clientX + "px";
-            ta.style.top = e.clientY - "50" + "px";
-            isLink = (e.target.nodeName === "A");
-            if (isLink) document.getElementById("open").className = "";
-            board = document.getSelection().toString();
-            // chrome.runtime.sendMessage({
-            //     cmd: 'copy',
-            //     text: board
-            // });
-        } else {
-            ta.className = "muti-hide";
-        }
-    });
-    document.body.addEventListener("mousewheel", function(e) {
-        ta.className = "muti-hide";
-    });
-}
+
 //鼠标既不在目标, 又不在translate则隐藏, 或者鼠标滚动
 // StorageArea.get('copy', function(item) {
 
